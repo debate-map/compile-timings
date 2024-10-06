58b886865a52fb5e824140ed70b4d582c1465170
rishadbaniya <rbdroid01@gmail.com>
2024-10-06 18:40:50 +0545

    feat: add iteration through all raw htmls

--- compile_timing_extractor/src/main.rs
+++ compile_timing_extractor/src/main.rs
@@ -3,22 +3,30 @@ use chrono::NaiveDateTime;
 use clap::Parser;
 use regex::Regex;
 use serde::Serialize;
-use std::{fs, path::Path};
+use std::{
+	fs,
+	path::{Path, PathBuf},
+};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct CliArgs {
+	/// Directory with raw HTML files generated from `cargo build --timings`
+	/// (with their original filenames i.e not tampered after generating from `cargo build --timings`)
 	#[arg(short, long)]
-	input_file: String,
+	raw_html_files_dir: PathBuf,
 
+	/// JSON file to store processed HTML files timestamps
 	#[arg(short, long)]
-	tracker_file: String,
+	tracker_file: PathBuf,
 
+	/// Directory to store metadata JSON files(1 per HTML file)
 	#[arg(short, long)]
-	metadata_dir: String,
+	metadata_dir: PathBuf,
 
+	/// Directory to store build units JSON files(1 per HTML file)
 	#[arg(short, long)]
-	units_data_dir: String,
+	units_data_dir: PathBuf,
 }
 
 #[derive(Debug, Serialize)]
@@ -57,63 +65,66 @@ fn extract_value<'a>(content: &'a str, pattern: &str) -> Option<&'a str> {
 	Regex::new(pattern).ok()?.captures(content).and_then(|cap| cap.get(1)).map(|m| m.as_str())
 }
 
-fn ensure_directory_exists(path: &Path) -> anyhow::Result<()> {
-	fs::create_dir_all(path).context("Failed to create directory")
-}
-
-fn ensure_tracker_file_exists(path: &Path) -> anyhow::Result<()> {
-	if !path.exists() {
-		write_json_file(path, &Vec::<String>::new())?;
-	}
-	Ok(())
-}
-
-fn write_json_file<T: Serialize>(path: &Path, data: &T) -> anyhow::Result<()> {
+fn write_json_file<P: AsRef<Path>, T: Serialize>(path: P, data: &T) -> anyhow::Result<()> {
 	let json = serde_json::to_string(data)?;
 	fs::write(path, json).context("Failed to write JSON file")
 }
 
 fn main() -> anyhow::Result<()> {
 	let args = CliArgs::parse();
-	let input_path = Path::new(&args.input_file);
-	let tracker_path = Path::new(&args.tracker_file);
-	let metadata_dir = Path::new(&args.metadata_dir);
-	let units_dir = Path::new(&args.units_data_dir);
 
-	ensure_directory_exists(metadata_dir)?;
-	ensure_directory_exists(units_dir)?;
-	ensure_tracker_file_exists(tracker_path)?;
-
-	let input_filename = input_path.file_name().context("Invalid input filename")?.to_str().unwrap();
-	let html_content = fs::read_to_string(input_path).context("Failed to read input file")?;
-
-	let mut tracker: BuildTracker = serde_json::from_str(&fs::read_to_string(tracker_path)?)?;
-
-	let raw_time = extract_value(&input_filename, r"(\d{8}T\d{6}Z)").unwrap();
-	let build_start_unix_timestamp = {
-		let parsed = NaiveDateTime::parse_from_str(raw_time, "%Y%m%dT%H%M%SZ")?.and_utc();
-		parsed.timestamp() as u64
-	};
-
-	if tracker.contains(&raw_time.to_string()) {
-		anyhow::bail!("File with this timestamp has already been processed");
+	if !args.raw_html_files_dir.exists() {
+		anyhow::bail!("Directory with raw HTML files does not exist");
 	}
 
-	let build_metadata = BuildMetadata {
-		total_time: extract_value(&html_content, r"<td>Total time:</td><td>(\d+(?:\.\d+)?)s").unwrap().parse()?,
-		rustc_version: extract_value(&html_content, r"<td>rustc:</td><td>(rustc [\d\.\w-]+)").unwrap(),
-		total_units: extract_value(&html_content, r"<td>Total units:</td><td>(\d+)").unwrap().parse()?,
-		build_start_unix_timestamp,
-	};
+	if !args.tracker_file.exists() {
+		write_json_file(&args.tracker_file, &Vec::<String>::new())?;
+	}
 
-	let units_data = extract_units_data(&html_content);
+	if !args.metadata_dir.exists() {
+		anyhow::bail!("Metadata directory does not exist");
+	}
 
-	write_json_file(&metadata_dir.join(format!("metadata_{raw_time}.json")), &build_metadata)?;
+	if !args.units_data_dir.exists() {
+		anyhow::bail!("Units data directory does not exist");
+	}
 
-	write_json_file(&units_dir.join(format!("units_{raw_time}.json")), &units_data)?;
+	let raw_html_files = fs::read_dir(&args.raw_html_files_dir)?.map(|entry| entry.map(|e| e.path())).collect::<Result<Vec<_>, _>>()?;
 
-	tracker.push(raw_time.to_string());
-	write_json_file(tracker_path, &tracker)?;
+	if raw_html_files.is_empty() {
+		anyhow::bail!("No files found in the directory");
+	}
+
+	let mut tracker: BuildTracker = serde_json::from_str(&fs::read_to_string(&args.tracker_file)?)?;
+
+	for raw_html_file in raw_html_files {
+		let input_filename = raw_html_file.file_name().context("Invalid input filename")?.to_str().unwrap();
+		let raw_time = extract_value(&input_filename, r"(\d{8}T\d{6}Z)").unwrap();
+
+		if !tracker.contains(&raw_time.to_string()) {
+			let build_start_unix_timestamp = {
+				let parsed = NaiveDateTime::parse_from_str(raw_time, "%Y%m%dT%H%M%SZ")?.and_utc();
+				parsed.timestamp() as u64
+			};
+
+			let html_content = fs::read_to_string(&raw_html_file).context("Failed to read input file")?;
+
+			let build_metadata = BuildMetadata {
+				total_time: extract_value(&html_content, r"<td>Total time:</td><td>(\d+(?:\.\d+)?)s").unwrap().parse()?,
+				rustc_version: extract_value(&html_content, r"<td>rustc:</td><td>(rustc [\d\.\w-]+)").unwrap(),
+				total_units: extract_value(&html_content, r"<td>Total units:</td><td>(\d+)").unwrap().parse()?,
+				build_start_unix_timestamp,
+			};
+
+			let units_data = extract_units_data(&html_content);
+
+			write_json_file(&args.metadata_dir.join(format!("metadata_{raw_time}.json")), &build_metadata)?;
+			write_json_file(&args.units_data_dir.join(format!("units_{raw_time}.json")), &units_data)?;
+			tracker.push(raw_time.to_string());
+		}
+	}
+
+	write_json_file(&args.tracker_file, &tracker)?;
 
 	Ok(())
 }
